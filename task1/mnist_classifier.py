5f1d9e07155d80adca8a562f4b7221e9ecbf2e02
sofeeka <sofiiahrychukh@gmail.com>
2025-02-17 19:49:27 +0200

    Implemented MnistClassifier

--- task1/mnist_classifier.py
+++ task1/mnist_classifier.py
@@ -1,8 +1,26 @@
+from task1.models.rf_classifier import RFClassifier
+from task1.models.cnn_classifier import CNNClassifier
+from task1.models.fnn_classifier import FNNClassifier
+
+
 class MnistClassifier:
     def __init__(self, algorithm):
         if algorithm not in ['cnn', 'rf', 'nn']:
             raise ValueError(f"Invalid algorithm '{algorithm}'.")
 
         self.algorithm = algorithm
+        self.__initialise_model__()
 
-            
+    def __initialise_model__(self):
+        if self.algorithm == 'rf':
+            self.model = RFClassifier()
+        elif self.algorithm == 'cnn':
+            self.model = CNNClassifier()
+        else:
+            self.model = FNNClassifier()
+
+    def train(self, x_train, y_train):
+        self.model.train(x_train, y_train)
+
+    def predict(self, x_test):
+        return self.model.predict(x_test)
