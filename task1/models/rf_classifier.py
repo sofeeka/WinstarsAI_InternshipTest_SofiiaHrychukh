17150d83d569a0a1f391f08817cfcbd7bc76cac2
sofeeka <sofiiahrychukh@gmail.com>
2025-02-17 19:50:01 +0200

    Implemented Random Forest Classifier

--- task1/models/rf_classifier.py
+++ task1/models/rf_classifier.py
@@ -1,12 +1,15 @@
 from mnist_classifier_interface import MnistClassifierInterface
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 
 class RFClassifier(MnistClassifierInterface):
+    def __init__(self, n_estimators=100):
+        self.n_estimators = n_estimators
+        self.model = RandomForestClassifier(n_estimators=self.n_estimators)
+
     def train(self, X_train: np.array, y_train: np.array):
-        # todo
-        pass
+        self.model.fit(X_train, y_train)
 
     def predict(self, X_test: np.array):
-        # todo
-        pass
+        return self.model.predict(X_test)
