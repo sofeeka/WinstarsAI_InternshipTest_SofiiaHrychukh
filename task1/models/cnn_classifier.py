64a69771d919e853f5f143a25ac93c2cd90146f1
sofeeka <sofiiahrychukh@gmail.com>
2025-02-18 02:56:08 +0200

    Implemented CNNClassifier

--- task1/models/cnn_classifier.py
+++ task1/models/cnn_classifier.py
@@ -1,12 +1,30 @@
 from task1.models.mnist_classifier_interface import MnistClassifierInterface
 import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
 
 
 class CNNClassifier(MnistClassifierInterface):
-    def train(self, X_train: np.array, y_train: np.array):
-        # todo
-        pass
+    def __init__(self):
+        self.model = keras.Sequential([
+            layers.Input(shape=(28, 28, 1)),
+            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Flatten(),
+            layers.Dense(128, activation='relu'),
+            layers.Dense(10, activation='softmax')
+        ])
 
-    def predict(self, X_test: np.array):
-        # todo
-        pass
+    def train(self, X_train: np.array, y_train: np.array):
+        X_train_reshaped = X_train.reshape(-1, 28, 28, 1).astype("float32")
+
+        self.model.compile(optimizer='adam',
+                           loss='sparse_categorical_crossentropy',
+                           metrics=['accuracy'])
+
+        self.model.fit(X_train_reshaped, y_train, epochs=5)
+
+    def predict(self, data: np.array):
+        return self.model.predict(data)
