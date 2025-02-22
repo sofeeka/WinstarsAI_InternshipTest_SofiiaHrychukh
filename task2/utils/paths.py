ead8baf8c6504eddae4f5d4b7f11a724e76e8ce3
sofeeka <sofiiahrychukh@gmail.com>
2025-02-22 11:47:46 +0200

    Create paths.py

---
+++ task2/utils/paths.py
@@ -0,0 +1,7 @@
+# model
+MODEL_NAME = 'ner_model'
+NER_MODEL_PATH = '../task2/' + MODEL_NAME
+
+# data
+NER_DATA_PATH = '../task2/data/ner_dataset_spacy.json'
+CV_DATA_PATH = '../task2/data/animals10'
