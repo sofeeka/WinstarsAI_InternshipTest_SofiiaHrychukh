c7b9882f819e36f59b00007d05dbc9e294ae97e1
sofeeka <sofiiahrychukh@gmail.com>
2025-02-23 23:49:39 +0200

    Update inference_ner.py

--- task2/utils/inference_ner.py
+++ task2/utils/inference_ner.py
@@ -1,6 +1,5 @@
 import os
 import spacy
-from pathlib import Path
 
 from task2.utils.train_ner import train_ner
 from task2.utils.paths import NER_MODEL_PATH
@@ -9,12 +8,9 @@ from task2.utils.paths import NER_MODEL_PATH
 def inference_ner(text):
     # check if model exists
     if not os.path.exists(NER_MODEL_PATH):
-        print('Model does not exist')
-        print('Training a model')
         # train new model
         nlp = train_ner()
     else:
-        print(f'Loading model at {NER_MODEL_PATH}')
         # load existing model
         nlp = spacy.load(NER_MODEL_PATH)
     doc = nlp(text)
